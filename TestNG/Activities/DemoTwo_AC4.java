887487b7cd005f39703e083aacba1b5a22ee0eb5
UzmaTarannum-BVRITH <30944097+UzmaTarannum-BVRITH@users.noreply.github.com>
2022-04-13 18:31:51 +0530

    Update and rename DemoTwo.java to DemoTwo_AC4.java

--- TestNG/Activities/DemoTwo_AC4.java
+++ TestNG/Activities/DemoTwo_AC4.java
@@ -1,7 +1,7 @@
 package suiteExample;
 import org.testng.annotations.Test;
 
-public class DemoTwo {
+public class DemoTwo_AC4 {
     @Test
     public void TestCase() {
         System.out.println("I'm in the test case from DemoTwo Class");
